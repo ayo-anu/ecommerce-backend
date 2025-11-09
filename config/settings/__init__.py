a2a7667595c3e87f3a88242c08a6dea7b9ebe8a2
bamgboseayoyimika-oss <bamgboseayoyimika@gmail.com>
2025-11-09 13:26:03 -0800

    Fix: Load correct settings based on ENVIRONMENT variable

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,15 +1,11 @@
-"""
-Django settings module selector based on environment
-"""
 import os
-from decouple import config
 
-# Get environment from .env file, default to development
-ENVIRONMENT = config('ENVIRONMENT', default='development')
+# Determine which settings to use
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 if ENVIRONMENT == 'production':
     from .production import *
-elif ENVIRONMENT == 'test':
-    from .test import *
+elif ENVIRONMENT == 'staging':
+    from .staging import *
 else:
     from .development import *
