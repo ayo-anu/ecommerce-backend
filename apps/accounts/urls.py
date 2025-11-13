31ea08e6b84a09247b65e1f98494bd7b96589949
bamgboseayoyimika-oss <bamgboseayoyimika@gmail.com>
2025-11-13 14:40:56 -0800

    Fix login endpoint to return user data

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from .views import (
     UserRegistrationView, UserViewSet, AddressViewSet, 
     PasswordResetViewSet, LoginView
@@ -12,12 +12,10 @@ router.register(r'addresses', AddressViewSet, basename='address')
 router.register(r'password-reset', PasswordResetViewSet, basename='password-reset')
 
 urlpatterns = [
-    # JWT Authentication
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # Authentication - Using custom LoginView that returns user data
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     
     # Registration
     path('register/', UserRegistrationView.as_view(), name='register'),
